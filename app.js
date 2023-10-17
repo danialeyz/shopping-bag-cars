d2b3762fef32b88b66df65577066425287fa79fb
danialeyz <danialeyvaziiii@gmail.com>
2023-10-17 18:57:34 +0330

    UI responsive 80% completed

--- app.js
+++ app.js
@@ -18,37 +18,70 @@ let products = [
   {
     id: 1,
     name: "PRODUCT NAME 1",
-    image: "1.PNG",
+    image: "1.webp",
     price: 120000,
   },
   {
     id: 2,
     name: "PRODUCT NAME 2",
-    image: "2.PNG",
+    image: "2.webp",
     price: 120000,
   },
   {
     id: 3,
     name: "PRODUCT NAME 3",
-    image: "3.PNG",
+    image: "3.jpg",
     price: 220000,
   },
   {
     id: 4,
     name: "PRODUCT NAME 4",
-    image: "4.PNG",
+    image: "4.webp",
     price: 123000,
   },
   {
     id: 5,
     name: "PRODUCT NAME 5",
-    image: "5.PNG",
+    image: "5.webp",
     price: 320000,
   },
   {
     id: 6,
     name: "PRODUCT NAME 6",
-    image: "6.PNG",
+    image: "6.webp",
+    price: 120000,
+  },
+
+  {
+    id: 7,
+    name: "PRODUCT NAME 6",
+    image: "6.webp",
+    price: 120000,
+  },
+  {
+    id: 7,
+    name: "PRODUCT NAME 6",
+    image: "7.webp",
+    price: 120000,
+  },
+  {
+    id: 8,
+    name: "PRODUCT NAME 6",
+    image: "8.webp",
+    price: 120000,
+  },
+
+  {
+    id: 9,
+    name: "PRODUCT NAME 6",
+    image: "9.webp",
+    price: 120000,
+  },
+
+  {
+    id: 10,
+    name: "PRODUCT NAME 6",
+    image: "10.webp",
     price: 120000,
   },
 ];
